f6ca8e5f1084f652f362e4a3f78e6f0442f22981
Daniel Choi <dhchoi@gmail.com>
2011-10-24 19:29:58 -0400

    git edit

--- app/controllers/front_edit_controller.rb
+++ app/controllers/front_edit_controller.rb
@@ -7,8 +7,31 @@ class FrontEditController < ::ApplicationController
 
 
   def index
-    file = params[:file]
-    @source_path = Rails.root + file
+    @source_path = Rails.root + params[:file]
     @source = File.read @source_path
+  end
+
+  def update
+    @path = params[:file]
+    n = params[:source].gsub(/\r\n/, "\n")
+    File.open(@path, 'w') {|f| f.write(n)}
+
+    # git commit
+
+    `git add #{@path}`
+    edit_msg = !params[:edit_message].blank? ? params[:edit_message] : "unspecified edit"
+
+    author = (session[:commit_author] = params[:commit_author])
+    if session[:commit_author].blank?
+      flash.now[:notice] = "Edited By must not be blank"
+      @source = params[:source]
+      render :action => 'index'
+      return
+    end
+    `git commit --author='#{author}' -m '#{edit_msg}' #{@path}`
+
+    params.delete(:source)
+    flash[:notice] = "#@path updated"
+    redirect_to :action => 'index', :file => params[:file]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
-
 Rails.application.routes.draw do
   get "/front_edit" => "front_edit#index"
+  post "/front_edit/update" => "front_edit#update"
 end
 
