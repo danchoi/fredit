925aa05047d24247448c20abf98299be864b1aac
Daniel Choi <dhchoi@gmail.com>
2011-10-24 18:05:42 -0400

    dropdown of views and assets

--- app/controllers/front_edit_controller.rb
+++ app/controllers/front_edit_controller.rb
@@ -1,15 +1,14 @@
 class FrontEditController < ::ApplicationController
+
   layout false
 
-  CSS_DIR = Rails.root + 'public/stylesheets/**/*'
-  JS_DIR = Rails.root + 'public/javascripts/**/*'
+  CSS_DIR = Rails.root + 'public/stylesheets/**/*.css'
+  JS_DIR = Rails.root + 'public/javascripts/**/*.js'
 
 
   def index
     file = params[:file]
     @source_path = Rails.root + file
     @source = File.read @source_path
-    @css = Dir[CSS_DIR].entries.map {|x| FrontEdit.rel_path(x)}
-    @js = Dir[JS_DIR].entries.map {|x| FrontEdit.rel_path(x)}
   end
 end
--- lib/front_edit.rb
+++ lib/front_edit.rb
@@ -1,17 +1,32 @@
 require 'front_edit/erb'
 
 module FrontEdit
+  extend self
 
   def self.rel_path(path)
     path.sub(Rails.root.to_s + '/', '')
   end
 
-  def self.link(path)
-    x = rel_path path
+  def self.rel_paths(paths)
+    paths.map {|x| rel_path(x)}
+  end
+
+  def self.link(x)
     s = <<-END
     <a href="/front_edit?file=#{x}">#{x}</a>
     END
     s.strip.html_safe
+  end
+
+  def self.entries(glob)
+    Dir[Rails.root + glob].entries.map {|e| rel_path(e)}
+  end
+
+  def self.editables
+    css = entries('public/stylesheets/**/*.css')
+    js = entries('public/javascripts/**/*.js')
+    views = entries('app/views/**/*.html.{erb,haml}')
+    {:css => css, :views => views, :javascript => js}
   end
 
 end
