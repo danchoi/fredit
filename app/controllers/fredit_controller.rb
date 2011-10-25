d3c7840a33fdaf083dd0b7d26acc713fc64a3b5c
Daniel Choi <dhchoi@gmail.com>
2011-10-25 10:25:45 -0400

    basic shell injection protection

--- app/controllers/fredit_controller.rb
+++ app/controllers/fredit_controller.rb
@@ -1,4 +1,3 @@
-require 'shellwords'
 require 'git'
 
 class FreditController < ::ApplicationController
@@ -18,10 +17,15 @@ class FreditController < ::ApplicationController
   def update
     @path = secure_path params[:file_path]
 
-    edit_msg = !params[:edit_message].blank? ? Shellwords.shellescape(params[:edit_message].gsub(/["']/, '')) : "unspecified edit"
+    edit_msg = !params[:edit_message].blank? ? params[:edit_message] : "unspecified edit"
+    edit_msg_file = Tempfile.new('commit-message')
+    t.write(edit_msg) # we write this message to a file to protect against shell injection
+    t.cloase
 
-    session[:commit_author] = (params[:commit_author] || '').gsub(/['"]/, '')
-    author = session[:commit_author]
+    session[:commit_author] = (params[:commit_author] || '')
+    # cleanup any shell injection attempt characters
+    author = session[:commit_author].gsub(/[^\w@<>. ]/, '') 
+
     if session[:commit_author].blank?
       flash.now[:notice] = "Edited By must not be blank"
       @source = params[:source]
@@ -32,14 +36,14 @@ class FreditController < ::ApplicationController
     if params[:commit] =~ /delete/i
       `git rm #@path`
       flash[:notice] = "#@path deleted"
-      res = system %Q|git commit --author='#{author}' -m '#{edit_msg}' #{@path}|
+      res = system %Q|git commit --author='#{author}' --file '#{edit_msg_file}' #{@path}|
       @path = nil
     else
       n = params[:source].gsub(/\r\n/, "\n")
       File.open(@path, 'w') {|f| f.write(n)}
       system %Q|git add #{@path}|
       flash[:notice] = "#@path updated"
-      res = system %Q|git commit --author='#{author}' -m '#{edit_msg}' #{@path}|
+      res = system %Q|git commit --author='#{author}' --file '#{edit_msg_file}' #{@path}|
     end
     if res == false
       flash[:notice] = "Something went wrong with git. Make sure you changed something and filled in required fields."
@@ -58,7 +62,7 @@ class FreditController < ::ApplicationController
 private
 
   def load_git
-    @git = Git.init
+    @git = Git.init Rails.root
   end
 
   def secure_path(path)
